830dd450358f6d48e52fdf54ef414bc0b5e8ce12
dakshesh19 <kashyapdakshesh@gmail.com>
2021-09-17 17:27:26 +0530

    task done

---
+++ task_done.py
@@ -0,0 +1,17 @@
+import json
+from datetime import date
+Date = date.today()
+print (Date)
+x = open("test.json")
+transactions = json.load(x)
+print (type(x))
+print (type(transactions))
+for k,v in transactions.items():
+    if k == "events":
+        for item in v:
+            for final,y in item.items():
+                if final == "event":
+                    if  y == "bot":
+                        print ("bot : {} " .format(item["text"]))
+                    if  y == "user":
+                        print ("user : {} " . format(item["text"]))
