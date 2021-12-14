b05dc19e4e8b71a6981f0a207727b3c36cd9cf2d
dakshesh19 <kashyapdakshesh@gmail.com>
2021-12-14 11:08:51 +0530

    vvv

--- transcribe_try.py
+++ transcribe_try.py
@@ -1,27 +1,18 @@
-gcs_uri ='gs://ags_2021/2286220210929142155867.wav'
+gcs_uri ='gs://ags_2021/2286220210929142155867(2).wav'
 from google.cloud import speech
-x = open("transcribe_enhanced_3" , "w")
-
+x = open("trancribe_2" , "w")
 client = speech.SpeechClient()
-
 audio = speech.RecognitionAudio(uri=gcs_uri)
 config = speech.RecognitionConfig(
     encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
     sample_rate_hertz=8000,
     language_code="en-US",
     use_enhanced=True,
-    # A model must be specified to use enhanced model.
     model="phone_call",
 )
-
 operation = client.long_running_recognize(config=config, audio=audio)
-
 print("Waiting for operation to complete...")
 response = operation.result(timeout=1000)
-
-# Each result is for a consecutive portion of the audio. Iterate through
-# them to get the transcripts for the entire audio file.
 for result in response.results:
-    # The first alternative is the most likely one for this portion.
     x.write(u"Transcript: {}\n".format(result.alternatives[0].transcript))
 x.close()
