7ec6ffbf9fae260027ac37d47e0822c7856cdf8a
dakshesh19 <kashyapdakshesh@gmail.com>
2021-12-21 16:13:42 +0530

    done

---
+++ try_1/actions/dbutils.py
@@ -0,0 +1,20 @@
+pizza_name = { 
+    "margarita" : "100",
+    "fresh pan" : "150",
+    "veggie special" : " 200",
+    "farmhouse" : "250",
+    "cheese burst" : "300"
+}
+size = {  
+    "small" : "1",
+    "medium" : "1.5",
+    "large" : "2"
+}
+extra_cheese = {
+    "cheese_burst" : "60"
+}
+side_order = {
+    "cold drink" : "30",
+    "bread sticks" : "50",
+    "cheesy dip" : "50"
+}
